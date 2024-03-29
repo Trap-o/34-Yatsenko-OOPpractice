2412565c04a59429dbb2a44bbafe19a9f514f194
Віталій Яценко <yatsiks69@gmail.com>
2024-03-29 12:31:23 +0200

    task1_task

--- src/pkg34/yatsenko/ooppractice/YatsenkoOOPpractice.java
+++ src/pkg34/yatsenko/ooppractice/YatsenkoOOPpractice.java
@@ -14,7 +14,19 @@ public class YatsenkoOOPpractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        String[] arrayOfMilitaryVehicle = new String[100000];
+        final String americanUnluckyNumber = "13";
+        final String chinaUnluckyNumber = "4";
+        int numberOfUnluckyVehicle = 0;
+
+        for (int i = 0; i < arrayOfMilitaryVehicle.length; i++) {
+            arrayOfMilitaryVehicle[i] = String.format("%05d", i);
+            if (arrayOfMilitaryVehicle[i].contains(americanUnluckyNumber)
+                    || arrayOfMilitaryVehicle[i].contains(chinaUnluckyNumber)) {
+                numberOfUnluckyVehicle++;
+            }
+        }
     
+        System.out.println(numberOfUnluckyVehicle);
+    }
 }
